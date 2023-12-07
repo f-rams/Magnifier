509a7389b6c055eb205604a3710eec36e6045c1e
Felipe Ramos <ffrf88@icloud.com>
2023-12-06 23:23:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {"pool_pre_ping": True,
-                                           "pool_recycle": 600, "poolclass": NullPool}
+                                           "pool_recycle": 300, "poolclass": NullPool}
 app.config['SQLALCHEMY_RESET_ON_RETURN'] = False
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
